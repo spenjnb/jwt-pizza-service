14dcc0efdd9a8dcd0f75bd8c6e5369b38460551c
Spencer <spenjnb@gmail.com>
2024-10-28 00:54:33 -0600

    Update files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const app = require('./service.js');
+const app = require("./service.js");
 
-const port = process.argv[2] || 3000;
+const port = process.argv[2] || 3001;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
